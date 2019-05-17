aeb479cc9a31a7c3c702e917c0320678338db2d2
kaden.burgart <kaden.burgart@gmail.com>
2019-05-16 17:07:07 -0700

    Created projet

---
+++ ConsoleZero/ConsoleZero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
